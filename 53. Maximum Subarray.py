68a7105f35bcf5468594ad02d4f4456684af1a68
vit_az <vit.azarov@gmail.com>
2022-06-12 15:29:23 -0300

    - Added recursive solution to problem 53.

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -1,8 +1,10 @@
+from tokenize import String
 from unittest import result
 from typing import List
 
 # https://leetcode.com/problems/maximum-subarray/
 
+# O(n) solution:
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
@@ -19,6 +21,28 @@ class Solution:
             globalSum = max(globalSum, currentSum)
 
         return globalSum
+
+
+#Recursive solution:
+class Solution:
+    globalMaxValue: int
+
+    def maxSubArray(self, nums: List[int]) -> int:
+        self.maxSubArrayRecursive(nums)
+        return globalMaxValue
+
+    def maxSubArrayRecursive(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            global globalMaxValue 
+            globalMaxValue = nums[0]
+            return nums[0]
+
+        lastElement = len(nums) - 1
+        currentValue = nums.pop()
+        currentMax = max(currentValue, currentValue + self.maxSubArrayRecursive(nums))
+        globalMaxValue = max(currentMax, globalMaxValue)
+        return currentMax
+
 
 solution = Solution()
 list = [-2,1,-3,4,-1,2,1,-5,4]
